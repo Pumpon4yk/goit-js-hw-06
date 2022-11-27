c17710dca1140adc6329a330ec7a4b322aeeab0d
Pumpon4yk <tt10392@gmail.com>
2022-11-27 18:27:23 +0100

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredEl = document.querySelector('#ingredients');
+
+function createElement (value){
+
+return value.map(el => {
+  const item = document.createElement("li")
+  item.textContent = `${el}`;
+  item.classList.add('item');
+
+  return item;
+})
+};
+
+const element = createElement(ingredients);
+
+ingredEl.append(...element)
