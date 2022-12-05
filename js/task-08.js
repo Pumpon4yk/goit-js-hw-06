54f00f98dca0000fab173d638585ccf616e016d7
Pumpon4yk <tt10392@gmail.com>
2022-12-05 22:10:24 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener("submit", submit);
+
+function submit(event) {
+    event.preventDefault();
+    const {elements: { email, password }} = event.currentTarget;
+
+    if (email.value === '' || password.value  === '') {
+        return alert('Всі поля повинні бути заповнені')
+    };
+    
+const value = {
+    email: email.value,
+    password: password.value,
+}
+    console.log(value);
+    
+    return form.reset();
+}
+
+
 
