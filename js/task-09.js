f41120a332245bf9ab46bc375ebdf524c773eeb2
Pumpon4yk <tt10392@gmail.com>
2022-12-07 23:54:47 +0100

    prittier

--- js/task-09.js
+++ js/task-09.js
@@ -2,14 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const body = document.querySelector('body');
-const btn = document.querySelector('.change-color');
-const span = document.querySelector('.color');
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
 
-btn.addEventListener('click', bgColor);
+btn.addEventListener("click", bgColor);
 
 function bgColor() {
   body.style.backgroundColor = getRandomHexColor();
   span.textContent = getRandomHexColor();
-
 }
--- js/task-10.js
+++ js/task-10.js
@@ -2,42 +2,39 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const input = document.querySelector('input');
-const create = document.querySelector('[data-create]');
-const destroy = document.querySelector('[data-destroy]');
-const divBox = document.querySelector('#boxes');
+const input = document.querySelector("input");
+const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const divBox = document.querySelector("#boxes");
 
-create.addEventListener('click', addBoxes)
-destroy.addEventListener('click', destroyBoxes)
-input.addEventListener('input', createBoxes)
+create.addEventListener("click", addBoxes);
+destroy.addEventListener("click", destroyBoxes);
+input.addEventListener("input", createBoxes);
 
-  let size = 20;
+let size = 20;
 
-function  createBoxes(amount){
+function createBoxes(amount) {
   // console.dir(amount.currentTarget.value);
-const num = amount.currentTarget.value
+  const num = amount.currentTarget.value;
   return num;
 }
 
 console.log(createBoxes());
 
-function addBoxes(){
+function addBoxes() {
   let divEls = ``;
   // const num = createBoxes();
 
   for (let i = 1; i <= createBoxes(); i += 1) {
-  let divEl = `<div  style="background-color: ${getRandomHexColor()};width: ${size}px; height: ${size}px;margin: 5px; "></div>`;
-  
-  size += 10;
-  divEls += divEl;
+    let divEl = `<div  style="background-color: ${getRandomHexColor()};width: ${size}px; height: ${size}px;margin: 5px; "></div>`;
+
+    size += 10;
+    divEls += divEl;
+  }
+
+  return (divBox.innerHTML = divEls);
 }
 
-return divBox.innerHTML = divEls;
+function destroyBoxes() {
+  return (divBox.innerHTML = "");
 }
-
-
-function destroyBoxes(){
-  return divBox.innerHTML = '';
-}
-
-
